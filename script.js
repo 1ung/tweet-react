3a79dddb676d449d0126a1c5e4d3ef03f74d01ec
1ung <leewhe@hotmail.com>
2018-05-03 21:03:21 +0800

    basic

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-var monkey = "hello";
-ReactDOM.render(
+// console.log(tweets);
+class Tweet extends React.Component {
+  render() {
+    return (
     <div>
-      {monkey}
-    </div>,
-    document.getElementById('root')
+      <h3>{this.props.name}</h3>
+      <p>{this.props.post}</p>
+    </div>  
+    );
+  }
+}
+
+class List extends React.Component {
+  render() {
+    let listOfTweets = this.props.tweets.map((tweet, index) => {
+      return <Tweet key={tweet.id} name={tweet.user.name} post={tweet.text}></Tweet>;
+    });
+      return (
+        <div>
+        {listOfTweets}
+        </div>
+      );
+    }
+}
+
+ReactDOM.render(
+  <List tweets={tweets} />,
+  document.getElementById('root')
 );
